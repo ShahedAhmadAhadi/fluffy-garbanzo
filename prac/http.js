f15f7015f8f534036d5860430573870d5218ccbd
Shahed Ahmad <shaheda.a16@gmail.com>
2021-12-05 17:47:34 +0430

    http requests to node server and package.json file added

--- prac/http.js
+++ prac/http.js
@@ -1,8 +1,15 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-    res.write('Welcome to my first node server app')
-    res.end()
+    if (req.url === '/') {
+        res.write('Home Page')
+        res.end()
+    }
+    if (req.url === '/about') {
+        res.write('about page')
+    }else {
+        res.end('<h1>no page like this!</h1>')
+    }
 })
 
 server.listen(8000)
