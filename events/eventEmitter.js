0275058e18912dc3ed2d2af182af225fe978198c
Shahed Ahmad <shaheda.a16@gmail.com>
2021-12-24 18:43:58 +0430

    events and os done with dev:

--- events/eventEmitter.js
+++ events/eventEmitter.js
@@ -2,10 +2,17 @@ const EventEmitter = require("events")
 const {createReadStream} = require('fs')
 
 const customEmitter = new EventEmitter()
-
 customEmitter.on('response', (name, age) => {
     console.log(`data recieved of ${name} has age of ${age} `)
 
 })
+customEmitter.on('data', () => {
+    console.log(`data emitter`)
+
+})
+
 
 customEmitter.emit('response', 'mike', 10)
+customEmitter.emit('data')
+console.log(customEmitter.eventNames())
+console.log(customEmitter.getMaxListeners())
