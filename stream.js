a18debb1fa6bd64d9475d6572fa49e47ed4ad931
Shahed Ahmad <shaheda.a16@gmail.com>
2021-12-28 14:57:08 +0430

    more on streams

--- stream.js
+++ stream.js
@@ -4,25 +4,35 @@ const server = require('http').createServer()
     
 
 server.on('request', (req, res) => {
-    fs.readFile('./dat.txt', (err, data) => {
-        if (err) {
-            console.log(err)
-        }
-        res.end(data)
-    })
+    // fs.readFile('./dat.txt', (err, data) => {
+    //     if (err) {
+    //         console.log(err)
+    //     }
+    //     res.end(data)
+    // })
+
+
+
+    // const readable = fs.createReadStream('./dat.txt')
+    // readable.on('data', chunk=> {
+    //     res.write(chunk)
+    // })
+
+    // readable.on('end', () => {
+    //     res.end('');
+    // })
+
+    // readable.on('error', (err) => {
+    //     console.log(err)
+    //     res.statusCode(500);
+    //     res.end('file not found!')
+    // })
+
+
+
 
     const readable = fs.createReadStream('./dat.txt')
-    readable.on('data', chunk=> {
-        res.write(chunk)
-    })
-
-    readable.on('end', () => {
-        res.end('');
-    })
-
-    readable.on('error', () => {
-        
-    })
+    readable.pipe(res)
 
 })
 
